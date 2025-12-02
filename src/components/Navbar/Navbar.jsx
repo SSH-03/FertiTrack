2cf5670cc2279c4bc6f90211e15b39f62355b28f
Midhun <midhunnagasaicse@gmail.com>
2025-12-03 01:56:49 +0530

    Some basic structure idea created in customers tab

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
                 <div class="container-fluid">
                     <a class="navbar-brand">
                         <img
-                            src={assets.FertiTrack_icon}
+                            src={assets.fertiTrack_icon}
                             alt="Bootstrap"
                             width="60"
                             height="60"
@@ -33,7 +33,8 @@ const Navbar = () => {
                     >
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <Link to={"/"}
+                                <Link
+                                    to={"/"}
                                     onClick={() => setMenu("customers")}
                                     class={`nav-link cursor-pointer${
                                         menu === "customers"
@@ -45,7 +46,8 @@ const Navbar = () => {
                                 </Link>
                             </li>
                             <li class="nav-item">
-                                <Link to={'/billing'}
+                                <Link
+                                    to={"/billing"}
                                     onClick={() => setMenu("billing")}
                                     class={`nav-link cursor-pointer${
                                         menu === "billing"
@@ -57,7 +59,8 @@ const Navbar = () => {
                                 </Link>
                             </li>
                             <li class="nav-item">
-                                <Link to={"/orders"}
+                                <Link
+                                    to={"/orders"}
                                     onClick={() => setMenu("orders")}
                                     class={`nav-link cursor-pointer${
                                         menu === "orders"
@@ -69,7 +72,8 @@ const Navbar = () => {
                                 </Link>
                             </li>
                             <li class="nav-item">
-                                <Link to={"/products"}
+                                <Link
+                                    to={"/products"}
                                     onClick={() => setMenu("products")}
                                     class={`nav-link cursor-pointer${
                                         menu === "products"
@@ -80,7 +84,43 @@ const Navbar = () => {
                                     Products
                                 </Link>
                             </li>
-                        </ul>
+                        </ul>{" "}
+                        <div class=" d-flex">
+                            <div>
+                                <Link
+                                    to={"/contact"}
+                                    onClick={() => setMenu("contact")}
+                                    class={`nav-link cursor-pointer${
+                                        menu === "contact"
+                                            ? "active border-bottom border-5 border-success"
+                                            : ""
+                                    }`}
+                                >
+                                    Contact For Application problems
+                                </Link>
+                            </div>
+
+                            <div >
+                                {" "}
+                                <img
+                                    className="ms-4"
+                                    src={assets.profile_icon}
+                                    // src={assets.Login_icon}
+                                    width="30"
+                                />
+                                <p>Profile</p>
+                                {/* <p>Login</p> */}
+                            </div>
+                            <div >
+                                {" "}
+                                <img
+                                    className="ms-4"
+                                    src={assets.logout_icon}
+                                    width="30"
+                                />
+                                <p>Logout</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </nav>
