b1a5f9f3cce658ca82144b1930e7247dfe637be8
Midhun <midhunnagasaicse@gmail.com>
2025-12-13 10:46:26 +0530

    Frontend for Login page is ready

--- src/components/LoginPopUp.jsx
+++ src/components/LoginPopUp.jsx
@@ -9,7 +9,10 @@ const LoginPopUp = ({ setShowLogin }) => {
                 <div className="position-relative">
                     <h2>{currState}</h2>
 
-                    <div className="  position-absolute top-0 end-0 hovermanual p-2 text-dark">
+                    <div
+                        className="  position-absolute top-0 end-0 hovermanual p-2 text-dark"
+                        onClick={() => setShowLogin(false)}
+                    >
                         <i class="bi bi-x-circle"></i>
                     </div>
                 </div>
@@ -17,30 +20,54 @@ const LoginPopUp = ({ setShowLogin }) => {
                     {currState === "Login" ? (
                         <></>
                     ) : (
-                        <input type="text" placeholder="Your Name" required />
+                        <input
+                            type="text"
+                            placeholder="Your Name"
+                            required
+                            className="form-control  mb-2"
+                        />
                     )}
 
-                    <input type="email" placeholder="Email" required />
-                    <input type="password" placeholder="Password" required />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        required
+                        className="form-control  mb-2"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        required
+                        className="form-control  mb-2"
+                    />
                 </div>
-                <button>
+
+                <button className="btn btn-outline-danger fw-bold fs-6">
                     {currState === "Sign Up" ? "Create account" : "Login"}
                 </button>
-                <div>
-                    <input type="checkbox" required />
-                    <p>I agree the terms and condition</p>
+
+                <div className="hstack from-group">
+                    <input type="checkbox" className="p-5" required></input>
+                    <p className="m-2">I agree the terms and condition</p>
                 </div>
+
                 {currState === "Login" ? (
                     <p>
                         Create a new account?{" "}
-                        <span onClick={() => setCurrState("Sign up")}>
+                        <span
+                            className="text-danger fw-bold fs-6 text-decoration-underline"
+                            onClick={() => setCurrState("Sign up")}
+                        >
                             Click here
                         </span>
                     </p>
                 ) : (
                     <p>
                         Already have a account?{" "}
-                        <span onClick={() => setCurrState("Login")}>
+                        <span
+                            className="text-danger fw-bold fs-6 text-decoration-underline"
+                            onClick={() => setCurrState("Login")}
+                        >
                             Login here
                         </span>
                     </p>
